03444bf7834847b71f54a96af95739bd592f6222
Bekzat Bagdatuly <100254408+roenbis@users.noreply.github.com>
2024-03-04 01:30:06 +0500

    f2f32f3d3d34f34f34f34f43r43f

--- validations.js
+++ validations.js
@@ -1,8 +1,20 @@
 import { body } from "express-validator";
 
+export const loginValidation = [
+    body('email', 'Incorrect format email').isEmail(),
+    body('password', 'Password must be minimum 5 symbols').isLength({ min: 5 }),
+];
+
 export const registerValidation = [
     body('email', 'Incorrect format email').isEmail(),
     body('fullName', 'Incorrect Name').isLength({ min: 3 }),
     body('password', 'Password must be minimum 5 symbols').isLength({ min: 5 }),
     body('avatarUrl', 'Wrong link').optional().isURL(),
+];
+
+export const postCreateValidation = [
+    body('title', 'Wrong title').isLength({min: 3}).isString(),
+    body('text', 'Wrong text').isLength({min: 3}).isString(),
+    body('tags', 'Wrong tag').isString(),
+    body('imageUrl', 'Wrong link').optional().isString(),
 ];
