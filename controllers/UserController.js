1537d98b27157e6ccad7a2aaa3f73acf34ee2f29
beks <bekzat.bagdatuly03@gmail.com>
2024-03-05 14:58:32 +0500

    backend

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,15 +1,12 @@
 import jwt from 'jsonwebtoken'
-import bcrypt, { hash } from 'bcrypt'
-import {validationResult} from "express-validator";
+import bcrypt from 'bcrypt'
+
 
 import UserModel from '../models/User.js'
 
 export const register = async (req, res) => {
     try {
-        const errors = validationResult(req);
-        if (!errors.isEmpty()) {
-            return req.status(400).json(errors.array());
-        }
+
 
         const password = req.body.password;
         const salt = await bcrypt.genSalt(10);
--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 import express from 'express'
+import multer from 'multer'
 import mongoose from "mongoose"
-import {registerValidation, loginValidation, postCreateValidation} from './validations.js'
-import * as UserController from './controllers/UserController.js';
-import * as PostController from './controllers/PostController.js';
-import checkAuth from './utils/checkAuth.js';
+
+import { registerValidation, loginValidation, postCreateValidation } from './validations.js'
+import { UserController, PostController } from './controllers/index.js'
+import { handleValidationErrors, checkAuth } from './utils/index.js'
 
 mongoose
     .connect('mongodb+srv://noobilis:lOedvJdGNR8xQo8R@cluster0.klsohvi.mongodb.net/blog?retryWrites=true&w=majority&appName=Cluster0')
@@ -12,17 +13,35 @@ mongoose
 
 const app = express();
 
-app.use(express.json());
+const storage = multer.diskStorage({
+    destination: (_, __, cb) => {
+        cb(null, 'uploads');
+    },
+    filename: (_, file, cb) => {
+        cb(null, file.originalname);
+    },
+});
 
-app.post('/auth/login', loginValidation, UserController.login);
-app.post('/auth/register', registerValidation, UserController.register);
+const upload = multer({ storage });
+
+app.use(express.json());
+app.use('/uploads', express.static('uploads'));
+
+app.post('/auth/login', loginValidation, handleValidationErrors, UserController.login);
+app.post('/auth/register', registerValidation, handleValidationErrors, UserController.register);
 app.get('/auth/me', checkAuth, UserController.getMe);
+
+app.post('/upload', checkAuth, upload.single('image'), (req, res) => {
+    res.json({
+        url: `/uploads/${req.file.originalname}`
+    });
+});
 
 app.get('/posts', PostController.getAll);
 app.get('/posts/:id', PostController.getOne);
-app.post('/posts', checkAuth, postCreateValidation, PostController.create);
+app.post('/posts', checkAuth, postCreateValidation, handleValidationErrors, PostController.create);
 app.delete('/posts/:id', checkAuth, PostController.remove);
-app.patch('/posts', PostController.update);
+app.patch('/posts/:id', checkAuth, postCreateValidation, handleValidationErrors, PostController.update);
 
 app.listen(1919, (err) => {
     if (err) return console.log(err);
